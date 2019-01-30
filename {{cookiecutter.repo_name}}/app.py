8d360a4c8fb3d3592c3594f86fe8cf72d412da5a
Donato Perconti <donato.perconti@gmail.com>
2019-01-30 15:53:57 -0800

    Add resource driven methods

--- {{cookiecutter.repo_name}}/app.py
+++ {{cookiecutter.repo_name}}/app.py
@@ -10,3 +10,28 @@ def alive():
     schema = AliveSchema(strict=True)
     result = schema.load({'alive': True})
     return Response(body=result.data, status_code=200)
+
+
+@app.route("/{{cookiecutter.resource}}", methods=['GET'])
+def get_{{cookiecutter.resource}}():
+    return Response(body={"Alive": True}, status_code=200)
+
+
+@app.route("/{{cookiecutter.resource}}", methods=['POST'])
+def get_{{cookiecutter.resource}}():
+    return Response(body={"Alive": True}, status_code=201)
+
+
+@app.route("/{{cookiecutter.resource}}/{{{cookiecutter.resource}}_id}", methods=['GET'])
+def get_stores({{cookiecutter.resource}}_id):
+    return Response(body={"{{cookiecutter.resource}}_id": {{cookiecutter.resource}}_id}, status_code=200)
+
+
+@app.route("/{{cookiecutter.resource}}/{{{cookiecutter.resource}}_id}", methods=['DELETE'])
+def get_stores({{cookiecutter.resource}}_id):
+    return Response(body={"{{cookiecutter.resource}}_id": {{cookiecutter.resource}}_id}, status_code=204)
+
+
+@app.route("/{{cookiecutter.resource}}/{{{cookiecutter.resource}}_id}", methods=['PATCH'])
+def get_stores({{cookiecutter.resource}}_id):
+    return Response(body={"{{cookiecutter.resource}}_id": {{cookiecutter.resource}}_id}, status_code=200)
